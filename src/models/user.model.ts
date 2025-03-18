22ebd1f1787043ec7f653b290b9245682054f60b
Sofiya Kalynovska <sofia.k@oxcompany.io>
2025-03-18 14:56:26 +0200

    Created user model

---
+++ src/models/user.model.ts
@@ -0,0 +1,18 @@
+import { model, Schema } from 'mongoose';
+
+const userSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    age: { type: Number, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: Number, required: true },
+    role: { type: String, required: true, default: ['user'] },
+    phone: { type: Number, required: false },
+    registrationDate: { type: Date, default: Date.now },
+    isDelete: { type: Boolean, default: false },
+    isVerified: { type: Boolean, default: false }
+  },
+  { timestamps: true, versionKey: false }
+);
+
+export const User = model('User', userSchema);
