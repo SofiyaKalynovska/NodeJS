6461b21793b02f9007f67ee6c3d14efd0ef2c651
Sofiya Kalynovska <sofia.k@oxcompany.io>
2025-03-20 14:31:24 +0200

    Changed linter configs

--- eslint.config.js
+++ eslint.config.js
@@ -1,29 +1,29 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import pluginReact from "eslint-plugin-react";
-import typescriptEslintParser from "@typescript-eslint/parser";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import pluginReact from 'eslint-plugin-react';
+import typescriptEslintParser from '@typescript-eslint/parser';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {
-    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
 
     languageOptions: {
-      parser: typescriptEslintParser, 
+      parser: typescriptEslintParser,
       parserOptions: {
-        ecmaVersion: 2023, 
-        sourceType: "module",
-      },
-    },
+        ecmaVersion: 2023,
+        sourceType: 'module'
+      }
+    }
   },
 
   {
     languageOptions: {
       globals: {
         ...globals.browser,
-        process: "readonly",
-      },
-    },
+        process: 'readonly'
+      }
+    }
   },
 
   pluginJs.configs.recommended,
@@ -32,31 +32,31 @@ export default [
   {
     settings: {
       react: {
-        version: "detect", 
-      },
-    },
+        version: 'detect'
+      }
+    }
   },
 
   {
     rules: {
-      "no-tabs": ["error"],
-      "indent": ["error", 2],
-      "no-trailing-spaces": ["error"],
-      "object-curly-spacing": ["error", "always"],
-      "quotes": ["error", "single"],
-      "semi": ["error", "always"],
-      "no-console": ["warn", { "allow": ["warn", "error"] }],
-      "space-before-function-paren": ["error", "always"],
-      "space-in-parens": ["error", "never"],
-      "brace-style": ["error", "1tbs"],
-      "linebreak-style": ["error", "unix"],
-      "camelcase": ["error"],
-      "no-multi-spaces": ["error"],
-      "comma-dangle": ["error", "never"],
-      "no-extra-semi": ["error"],
-      "no-extra-parens": ["error", "all"],
-      "prefer-const": "error",
-      "eqeqeq": ["error", "always"],
-    },
-  },
+      'no-tabs': ['error'],
+      indent: ['error', 2],
+      'no-trailing-spaces': ['error'],
+      'object-curly-spacing': ['error', 'always'],
+      quotes: ['error', 'single'],
+      semi: ['error', 'always'],
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
+      'space-before-function-paren': ['error', 'always'],
+      'space-in-parens': ['error', 'never'],
+      'brace-style': ['error', '1tbs'],
+      'linebreak-style': ['error', 'unix'],
+      camelcase: ['error'],
+      'no-multi-spaces': ['error'],
+      'comma-dangle': ['error', 'never'],
+      'no-extra-semi': ['error'],
+      'no-extra-parens': ['error', 'all'],
+      'prefer-const': 'error',
+      eqeqeq: ['error', 'always']
+    }
+  }
 ];
