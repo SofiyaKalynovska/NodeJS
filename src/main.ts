f0cf43e0d1bb9c652ce796965c1bdbfde55d3da7
Sofiya Kalynovska <sofia.k@oxcompany.io>
2025-03-20 14:33:33 +0200

    Added tasks route to main

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,16 @@
 import express, { NextFunction, Request, Response } from 'express';
-import { ApiError } from './errors/api-error';
 import { userRouter } from './routers/user.router';
 import { config } from './configs/config';
 import mongoose from 'mongoose';
+import { taskRouter } from './routers/task.router';
+import { ApiError } from './errors/api-error';
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/users', userRouter);
+app.use('/tasks', taskRouter);
 
 app.use(
   '*',
