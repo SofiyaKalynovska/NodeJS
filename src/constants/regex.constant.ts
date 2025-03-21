f127fb6a76ee40469bfb0d7fea7102640bc621a1
Sofiya Kalynovska <sofia.k@oxcompany.io>
2025-03-21 12:58:34 +0200

    Created regex constants

---
+++ src/constants/regex.constant.ts
@@ -0,0 +1,5 @@
+export const EMAIL_REGEX = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+export const PASSWORD_REGEX = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+
+export const PHONE_REGEX = /^\d{10}$/;
