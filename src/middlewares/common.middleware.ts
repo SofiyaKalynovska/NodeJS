231620771e6c22c857f334ffa5d5c8b3d857c606
Sofiya Kalynovska <sofia.k@oxcompany.io>
2025-03-21 13:01:38 +0200

    Removed error logic from common middleware

--- src/middlewares/common.middleware.ts
+++ src/middlewares/common.middleware.ts
@@ -1,6 +1,7 @@
 import { isObjectIdOrHexString } from 'mongoose';
 import { ApiError } from '../errors/api-error';
 import { NextFunction, Request, Response } from 'express';
+import { ObjectSchema } from 'joi';
 
 class CommonMiddleware {
   public isIdValid (key: string) {
@@ -16,26 +17,15 @@ class CommonMiddleware {
       }
     };
   }
-  public validateBody (fields: string[]) {
-    return (req: Request, res: Response, next: NextFunction) => {
-      try {
-        const dto: any = req.body;
-        fields.forEach((field) => {
-          if (!dto?.[field]) {
-            throw new ApiError(`${field} is required`, 400);
-          }
-          if (field === 'email' && !dto.email.includes('@')) {
-            throw new ApiError('Invalid email', 400);
-          }
-          if (field === 'password' && dto.password.length < 8) {
-            throw new ApiError(
-              'Password must be at least 8 characters long',
-              400
-            );
-          }
-        });
 
-        (req.body as any) = dto;
+  //If i dont have mongoose validation in model - i will use body validation
+  public validateBody (validator: ObjectSchema) {
+    return async (req: Request, res: Response, next: NextFunction) => {
+      try {
+        const validatedData = await validator.validateAsync(req.body);
+
+        req.body = validatedData;
+
         next();
       } catch (error) {
         next(error);
