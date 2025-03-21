0b6b8b623ffd5b939ed45844daa97d88f123857a
Sofiya Kalynovska <sofia.k@oxcompany.io>
2025-03-21 11:20:01 +0200

    Remover user data validation from router

--- src/routers/user.router.ts
+++ src/routers/user.router.ts
@@ -1,32 +1,30 @@
-import { NextFunction, Request, Response, Router } from 'express';
+import { Router } from 'express';
 import { userController } from '../controllers/user.controller';
-import { ApiError } from '../errors/api-error';
-
-const validateUserData = (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const dto = req.body;
-    if (!dto.name || dto.name.length < 3) {
-      throw new ApiError('Name and email are required', 400);
-    }
-    if (!dto.email || !dto.email.includes('@')) {
-      throw new ApiError('Invalid email', 400);
-    }
-    if (!dto.password || dto.password.length < 8) {
-      throw new ApiError('Password must be at least 8 characters long', 400);
-    }
-    req.body = dto;
-    next();
-  } catch (error) {
-    next(error);
-  }
-};
+import { commonMiddleware } from '../middlewares/common.middleware';
 
 const router = Router();
 // / because in main we are importing userRouter for '/user' endpoint
 router.get('/', userController.getAllUsers);
-router.get('/:userId', userController.getUserById);
-router.post('/', validateUserData, userController.createUser);
-router.patch('/:userId', validateUserData, userController.patchUser);
-router.delete('/:userId', userController.deleteUser);
+router.get(
+  '/:userId',
+  commonMiddleware.isIdValid('userId'),
+  userController.getUserById
+);
+router.post(
+  '/',
+  commonMiddleware.validateBody(['email', 'password', 'name']),
+  userController.createUser
+);
+router.patch(
+  '/:userId',
+  commonMiddleware.isIdValid('userId'),
+  commonMiddleware.validateBody(['email', 'password', 'name']),
+  userController.patchUser
+);
+router.delete(
+  '/:userId',
+  commonMiddleware.isIdValid('userId'),
+  userController.deleteUser
+);
 
 export const userRouter = router;
