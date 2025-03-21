dd6af4d7873d0dcbe6df2859bcdbe74bb68bc4c9
Sofiya Kalynovska <sofia.k@oxcompany.io>
2025-03-21 13:00:31 +0200

    Used validators in routers

--- src/routers/task.router.ts
+++ src/routers/task.router.ts
@@ -1,15 +1,15 @@
 import { taskController } from '../controllers/task.controller';
 import { Router } from 'express';
-import { validateTaskData } from '../middlewares/task.middleware';
 import { commonMiddleware } from '../middlewares/common.middleware';
+import { TaskValidator } from '../validators/task.validator';
 
 const router = Router();
 
-router.post('/', validateTaskData, taskController.createTask);
+router.post('/', commonMiddleware.validateBody(TaskValidator.createSchema) , taskController.createTask);
 router.get('/', taskController.getAllTasks);
 router.get('/:taskId',commonMiddleware.isIdValid('taskId'), taskController.getTaskById);
 router.get('/user/:userId', commonMiddleware.isIdValid('userId'), taskController.getTasksByUserId);
-router.patch('/:taskId', commonMiddleware.isIdValid('taskId'), validateTaskData, taskController.updateTask);
+router.patch('/:taskId', commonMiddleware.isIdValid('taskId'), commonMiddleware.validateBody(TaskValidator.patchSchema), taskController.updateTask);
 router.delete('/:taskId', commonMiddleware.isIdValid('taskId'), taskController.deleteTask);
 
 export const taskRouter = router;
