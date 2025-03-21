87278d21d2db894b215f4d42daf96aa48acee916
Sofiya Kalynovska <sofia.k@oxcompany.io>
2025-03-21 16:41:49 +0200

    Added sign up logic to auth

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,8 +1,16 @@
 import { ApiError } from '../errors/api-error';
+import { IUser } from '../interfaces/user.interface';
 import { userRepository } from '../repositories/user.repository';
 import { passwordService } from './password.service';
+import { userService } from './user.service';
 
 class AuthService {
+  public async signUp (dto: IUser): Promise<IUser> {
+    await userService.isEmailUnique(dto.email);
+    const password = await passwordService.hashPassword(dto.password);
+    return await userRepository.createUser({ ...dto, password });
+  }
+
   public async signIn (dto: any): Promise<any> {
     const user = await userRepository.getUserByEmail(dto.email);
     if (!user) {
@@ -17,6 +25,7 @@ class AuthService {
       throw new ApiError('Invalid email or password', 401);
     }
   }
+
 }
 
 export const authService = new AuthService();
