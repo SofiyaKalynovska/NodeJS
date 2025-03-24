78b8e3d4c43c60cb90c2a217c15b8ea44a7580b0
Sofiya Kalynovska <sofia.k@oxcompany.io>
2025-03-24 11:26:42 +0200

    Added tokens to config

--- src/configs/config.ts
+++ src/configs/config.ts
@@ -4,6 +4,9 @@ dotenv.config();
 
 export const config = {
   port: process.env.PORT || 3000,
-  mongoUri: process.env.MONGODB_URI || 'mongodb:'
-
+  mongoUri: process.env.MONGODB_URI || 'mongodb:',
+  jwtAccessSecret: process.env.JWT_ACCESS_SECRET,
+  jwtAccessExpiresIn: process.env.JWT_ACCESS_EXPIRES_IN,
+  jwtRefreshSecret: process.env.JWT_REFRESH_SECRET,
+  jwtRefreshExpiresIn: process.env.JWT_REFRESH_EXPIRES_IN
 };
