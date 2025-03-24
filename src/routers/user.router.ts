8a0635cb8f4b58b56ccf0170cc7afb73ee8d2d2b
Sofiya Kalynovska <sofia.k@oxcompany.io>
2025-03-24 11:37:38 +0200

    Minor changes

--- src/routers/user.router.ts
+++ src/routers/user.router.ts
@@ -2,25 +2,32 @@ import { Router } from 'express';
 import { userController } from '../controllers/user.controller';
 import { commonMiddleware } from '../middlewares/common.middleware';
 import UserValidator from '../validators/user.validator';
+import { authMiddleware } from '../middlewares/auth.middleware';
 
 const router = Router();
 // / because in main we are importing userRouter for '/user' endpoint
 router.get('/', userController.getAllUsers);
 router.get(
-  '/:userId',
-  commonMiddleware.isIdValid('userId'),
+  '/me',
+  authMiddleware.checkAccessToken,
   userController.getUserById
 );
 router.patch(
-  '/:userId',
-  commonMiddleware.isIdValid('userId'),
+  '/me',
+  authMiddleware.checkAccessToken,
   commonMiddleware.validateBody(UserValidator.userUpdateValidationSchema),
   userController.patchUser
 );
 router.delete(
-  '/:userId',
-  commonMiddleware.isIdValid('userId'),
+  '/me',
+  authMiddleware.checkAccessToken,
   userController.deleteUser
+);
+router.get(
+  '/:userId',
+  authMiddleware.checkAccessToken,
+  commonMiddleware.isIdValid('userId'),
+  userController.getUserById
 );
 
 export const userRouter = router;
