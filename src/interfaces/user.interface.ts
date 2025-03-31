cee52434c56af9e88209ecc8cd48070d55d79135
Sofiya Kalynovska <sofia.k@oxcompany.io>
2025-03-31 15:03:20 +0300

    Added interfaces

--- src/interfaces/user.interface.ts
+++ src/interfaces/user.interface.ts
@@ -2,22 +2,31 @@ import { RoleEnum } from '../enums/role.enum';
 import { ITask } from './task.interface';
 
 export interface IUser {
-    _id: string;
-    name: string;
-    email: string;
-    password: string;
-    role: RoleEnum;
-    phone?: string;
-    isDelete: boolean;
-    isVerified: boolean;
-    createdAt: Date;
-    updatedAt: Date;
-    tasks: ITask[] | null;
-
+  _id: string;
+  name: string;
+  email: string;
+  password: string;
+  role: RoleEnum;
+  phone?: string;
+  isDelete: boolean;
+  isVerified: boolean;
+  createdAt: Date;
+  updatedAt: Date;
+  tasks: ITask[] | null;
 }
 
-export type IUserCreateDto = Pick<IUser, 'name' | 'email' | 'password' | 'phone' | 'role'>;
+export type IUserCreateDto = Pick<
+  IUser,
+  'name' | 'email' | 'password' | 'phone' | 'role'
+>;
 
-export type IUserUpdateDto = Pick<IUser, 'name' | 'email' | 'password' | 'phone' | 'role' | 'isDelete' | 'isVerified'>;
+export type IUserUpdateDto = Pick<
+  IUser,
+  'name' | 'email' | 'password' | 'phone' | 'role' | 'isDelete' | 'isVerified'
+>;
 
-export type ILogin = Pick<IUser, 'email' | 'password'>
+export type ILogin = Pick<IUser, 'email' | 'password'>;
+
+export interface IRequestWithUser extends Request {
+  user: IUser;
+}
